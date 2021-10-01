eaefa2b084316ceca2b2f8f4d8cc7d746e48c63f
CAEUser <mail@rwth>
2021-10-01 22:11:53 +0000

    a

---
+++ db/130_create_tables.sql
@@ -0,0 +1,17 @@
+--
+-- Database Schema:  music
+-- Automatically generated sql script for the service 130, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table songs.
+--
+CREATE TABLE music.songs (
+  title String ,
+  id String ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+ 
+);
+
+
+
